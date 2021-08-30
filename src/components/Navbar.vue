0fe6f70bdb9350d3a87f0628a7e54ada50330994
andre-savedra <andre_savedra@hotmail.com>
2021-08-30 11:29:20 -0300

    final_30-08

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/history">History</router-link>
+        <router-link to="/network">Rede</router-link>
+        <router-link to="/work">Trabalhe Conosco</router-link>
+        <router-link to="/menu">Cardápio</router-link>  
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
+
+
